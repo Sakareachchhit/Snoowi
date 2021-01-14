4f338b3c94739e2b6f28072a625a53bbb13fcbf2
Sakareach CHHIT (IT) <sakareach_chhit@phillipbank.com.kh>
2021-01-14 13:33:05 +0700

    init prj

---
+++ src/app/store.tsx
@@ -0,0 +1,16 @@
+import {createStore, applyMiddleware} from 'redux';
+// import createSagaMiddleware from 'redux-saga';
+
+// const sagaMiddleware = createSagaMiddleware();
+// const middleware = [sagaMiddleware];
+
+// const composeEnhancers = composeWithDevTools({});
+
+// const store = createStore(
+//   rootReducer,
+//   composeEnhancers(applyMiddleware(...middleware)),
+// );
+
+// sagaMiddleware.run(rootSaga);
+
+// export default store;
